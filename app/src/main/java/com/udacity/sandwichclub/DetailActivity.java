3f7c4efd994bfb60192441839fff14f270df9b46
Sandeep Pandey <9078554+Codextor@users.noreply.github.com>
2018-07-31 00:09:03 +0530

    Update DetailActivity
    Update DetailActivity layout and populate it with details of sandwich

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -10,6 +10,13 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import android.widget.TextView;
+import android.text.TextUtils;
+
+import java.util.List;
+
+import android.graphics.Color;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
@@ -43,7 +50,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +63,48 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView name_tv = findViewById(R.id.name_tv);
+        name_tv.setText(sandwich.getMainName());
 
+        TextView also_known_tv = findViewById(R.id.also_known_tv);
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        String alsoKnownAs;
+        if (!alsoKnownAsList.isEmpty()) {
+            alsoKnownAs = TextUtils.join(", ", alsoKnownAsList);
+        } else {
+            alsoKnownAs = "alternate names not found";
+            also_known_tv.setTextColor(Color.parseColor("#C51162"));
+        }
+        also_known_tv.setText(alsoKnownAs);
+
+        TextView origin_tv = findViewById(R.id.origin_tv);
+        String placeOfOrigin = sandwich.getPlaceOfOrigin();
+        if (!placeOfOrigin.isEmpty()) {
+            origin_tv.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            origin_tv.setText("place of origin not found");
+            origin_tv.setTextColor(Color.parseColor("#C51162"));
+        }
+
+        TextView description_tv = findViewById(R.id.description_tv);
+        String description = sandwich.getDescription();
+        if (!description.isEmpty()) {
+            description_tv.setText(sandwich.getDescription());
+        } else {
+            description_tv.setText("description not found");
+            description_tv.setTextColor(Color.parseColor("#C51162"));
+        }
+
+        TextView ingredients_tv = findViewById(R.id.ingredients_tv);
+        List<String> ingredientsList = sandwich.getIngredients();
+        String ingredients;
+        if (!ingredientsList.isEmpty()) {
+            ingredients = TextUtils.join(", ", ingredientsList);
+        } else {
+            ingredients = "ingredients not found";
+            ingredients_tv.setTextColor(Color.parseColor("#C51162"));
+        }
+        ingredients_tv.setText(ingredients);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,49 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.*;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        //If json received is null, return null object
+        if (json == null) {
+            return null;
+        }
+        //Parse the json string received
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject name = sandwichJSON.getJSONObject("name");
+            String mainName = name.getString("mainName");
+
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsArray.get(i).toString());
+            }
+
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
+
+            String description = sandwichJSON.getString("description");
+
+            String image = sandwichJSON.getString("image");
+
+            JSONArray ingredientsArray = sandwichJSON.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredients.add(ingredientsArray.get(i).toString());
+            }
+
+            //Create a sandwich object with the parsed arguments and return it
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            Log.e("Sandwich Club", "unexpected JSON exception", e);
+            return null;
+        }
     }
 }
